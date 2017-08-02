39292afe6b36312eb0f97de9ad950c32d4bae279
xdk78 <xdk78@interia.pl>
2017-08-02 13:28:48 +0200

    Simplify api

--- README.md
+++ README.md
@@ -11,17 +11,18 @@ http://animesub.info
 ```javascript
 const animesub = require('animesub-api')
 const title = 'boruto'
-const filename = 'file'
+const filename = 'boruto_napisy' // output boruto_napisy.zip
 const titletype = {
-  org: 'org'
+  org: 'org', // oryginalny tytuł
+  pl: 'pl', // polski tytuł
+  en: 'en' // angielski tytuł
 }
-    
-const QUERY = {
-  id: 12,
-  sh: 13
-}
-    
-animesub.download(title, titletype.org, QUERY.id, QUERY.sh, filename)
+
+// id from 0 to 7
+let id = 1
+
+animesub.download(title, titletype.org, id, filename)
+
 ```
 
 or see example: https://github.com/xdk78/animesub-api/tree/master/example
--- index.js
+++ index.js
@@ -8,18 +8,13 @@ const cookieJar = new tough.CookieJar()
 let qs = require('querystring')
 
 let api = axios.create({
-  // baseURL: 'http://animesub.info/szukaj.php?szukane=boruto&pTitle=org',
   jar: cookieJar, // tough.CookieJar or boolean
   withCredentials: true // If true, send cookie stored in jar
 })
 
-// let SEARCH_URL = 'http://animesub.info/szukaj.php?szukane=boruto&pTitle=org'
-// let title = 'boruto'
-// let title_type ='org'
-
 const DOWNLOAD_URL = 'http://animesub.info/sciagnij.php'
 
-const download = (title, titletype, id, sh, filename) => {
+const download = (title, titletype, id, filename) => {
   api.get('http://animesub.info/szukaj.php?szukane=' + title + '&pTitle=' + titletype)
     .then(function (response) {
       console.log('\n GET')
@@ -32,18 +27,35 @@ const download = (title, titletype, id, sh, filename) => {
         if (err) {
           console.log(err)
         }
-        console.log(obj.value + '\n' + 'id: ' + obj.value[12] + '\n' + 'value: ' + obj.value[13])
 
-        // 1,,,ok,Zaloguj si�,,Szukaj,boruto,Szukaj napis�w,63072,a2b6a0f69b429902117c279e5a7c3a866412fe49,Pobierz napisy,63108,5ade033d2dd41bf1139186c57356d18a86cd44f0,Pobierz napisy,63317,bde916a1b94b68151b05b4f929bb2af4cb0c0330,Pobierz napisy,63328,d37437bf71eb50741d36f81d57fa24e2f1adcffa,Pobierz napisy,63418,fccfa09ab3be8bc0667612731596781cf62b4a16,Pobierz napisy,63454,e29a547218e83ec43fec03dda36665dc4e848885,Pobierz napisy,63455,6fa1270a94a3734eac43d74f474e11396eb7e13b,Pobierz napisy,63485,db53395c998a05341d09c10ebc8038aa78f96492,Pobierz napisy
-        // 9 = id = '63072'
-        // 10 = sh = 'a2b6a0f69b429902117c279e5a7c3a866412fe49'
-        // 11 = single_file = 'Pobierz napisy
+        function removeA (arr) {
+          let what, a, L, ax
+          a = arguments
+          L = a.length
+          while (L > 1 && arr.length) {
+            what = a[--L]
+            while ((ax = arr.indexOf(what)) !== -1) {
+              arr.splice(ax, 1)
+            }
+          }
+          return arr
+        }
+
+        let array = obj.value
+        removeA(array, 'ok', '1', 'Zaloguj si�', 'Szukaj', 'Szukaj napis�w', 'Pobierz napisy')
+        array.splice(0, 4)
+        console.log('All values: ' + array)
+
+        if (id === 0) {
+          id = 0
+        } else { id *= 2 }
 
         const QUERY = {
           id: obj.value[id],
-          sh: obj.value[sh]
+          sh: obj.value[id + 1]
         }
 
+        console.log('id:' + QUERY.id, 'sh: ' + QUERY.sh)
         api.post(DOWNLOAD_URL,
           qs.stringify({
             id: QUERY.id,

21bae718533939aed70b81f18ae34669afe79831
xdk78 <xdk78@interia.pl>
2018-05-17 22:12:22 +0200

    Move to grabbi scraper instead x-ray, update deps

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 const fs = require('fs')
-const x = require('x-ray')()
 const axios = require('axios')
 const axiosCookieJarSupport = require('@3846masa/axios-cookiejar-support').default
 const tough = require('tough-cookie')
-const qs = require('querystring')
+const qs = require('qs')
 const _ = require('lodash')
+const grabbi = require('grabbi')
 axios.defaults.adapter = require('axios/lib/adapters/http')
 const cookieJar = new tough.CookieJar()
 axiosCookieJarSupport(axios)
 
-const api = axios.create({
+const config = {
   baseURL: 'http://animesub.info',
   jar: cookieJar,
   withCredentials: true
-})
+}
+const api = axios.create(config)
 
 const DOWNLOAD_URL = '/sciagnij.php'
 const SEARCH_URL = '/szukaj.php?szukane='
@@ -35,42 +36,35 @@ const download = async (id, sh, path) => {
 
 const search = async (title, titletype, page) => {
   try {
-    let data = null
     if (page === undefined || page === null) {
       page = 0
     }
-    const response = await api.get(SEARCH_URL + title + '&pTitle=' + titletype + '&od=' + page)
-    x(response.data,
-      {
-        value: ['input@value'],
-        title: ['tr[class="KNap"] > td[width="45%"]'],
-        pages: 'td[class="MaleLtr"]:nth-of-type(1)'
-      }
-    )((err, obj) => {
-      if (err) {
-        throw new Error(err)
-      }
-      const queries = _.without(obj.value, 'ok', '1', 'Zaloguj si�', 'Szukaj', 'Szukaj napis�w', 'Pobierz napisy')
-      queries.splice(0, 4)
-      const out = obj.title.map((el, i) => {
-        if (i === 0) {
-          i = 0
-        } else { i *= 2 }
-        return ({
-          title: el,
-          id: queries[i],
-          sh: queries[i + 1]
-        })
+    const { res } = await grabbi(SEARCH_URL + title + '&pTitle=' + titletype + '&od=' + page, config)
+    const obj = {
+      input: [...res.querySelectorAll('input')],
+      title: [...res.querySelectorAll('tr[class="KNap"] > td[width="45%"]')],
+      pages: res.querySelector('td[class="MaleLtr"]:nth-of-type(1)')
+    }
+    const inputs = obj.input.map(el => el.value)
+    const queries = _.without(inputs, 'ok', '1', 'Zaloguj si�', 'Szukaj', 'Szukaj napis�w', 'Pobierz napisy')
+    queries.splice(0, 4)
+    const out = obj.title.map((el, i) => {
+      if (i === 0) {
+        i = 0
+      } else { i *= 2 }
+      return ({
+        title: el.innerHTML,
+        id: queries[i],
+        sh: queries[i + 1]
       })
-      const pages = _.words(obj.pages)
-      pages.splice(0, 2)
-      const outs = {
-        pages: Math.floor(pages[0] / 30),
-        page: page,
-        json: out
-      }
-      data = outs
     })
+    const pages = _.words(obj.pages.innerHTML)
+    pages.splice(0, 2)
+    const data = {
+      pages: Math.floor(pages[0] / 30),
+      page: page,
+      json: out
+    }
     return data
   } catch (err) {
     throw new Error(err)
